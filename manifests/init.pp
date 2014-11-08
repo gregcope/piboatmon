932a3a54071e80e4fe78d42ce952ba7bfbea1b18
gregcope <gregcope@gmail.com>
2014-11-08 20:47:27 +0000

    Install usb switch and lots of doconotes

---
+++ manifests/init.pp
@@ -0,0 +1,35 @@
+#
+#
+# Think I need to run;
+# usb_modeswitch -c /etc/usb_modeswitch.conf
+# http://debbox.dk/sending-sms-from-huawei-e1752/
+#
+#
+#
+
+
+#
+# huawei e1752cu
+# 
+# install usb-modeswitch
+# which basically sets some udev rules
+package { 'usb-modeswitch': }
+
+file { '/etc/usb-modeswitch.conf':
+  ensure => 'present',
+  content => 'DefaultVendor= 0×12d1\nDefaultProduct= 0×1446\nTargetVendor= 0×12d1\nTargetProdct= 0×1001\nMessageEndpoint= 0×01\nMessageContent= \"55534243000000000000000000000011060000000000000000000000000000\""
+}
+
+# or
+# http://ubuntuforums.org/showthread.php?t=1996734&p=12000198#post12000198
+
+# gammu?
+# http://www.mattiasnorell.com/send-sms-from-a-raspberry-pi/
+# http://wammu.eu/phones/huawei/4062/
+
+# http://myraspberryandme.wordpress.com/2013/09/13/short-message-texting-sms-with-huawei-e220/
+
+# gnokki? looks simple ...
+# http://debbox.dk/sending-sms-from-huawei-e1752/
+#
+
