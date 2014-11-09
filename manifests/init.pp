9cd2a9fee4e749057ee8ac4c6ecb3325455679b3
gregcope <gregcope@gmail.com>
2014-11-09 12:26:12 +0000

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ package { 'usb-modeswitch': }
 
 file { '/etc/usb_modeswitch.d/e1752cu.conf':
   ensure => 'present',
-  content => 'DefaultVendor= 0×12d1\nDefaultProduct= 0×1446\nTargetVendor= 0×12d1\nTargetProdct= 0×1001\nMessageEndpoint= 0×01\nMessageContent= \"55534243000000000000000000000011060000000000000000000000000000\""
+  content => "DefaultVendor= 0×12d1\nDefaultProduct= 0×1446\nTargetVendor= 0×12d1\nTargetProdct= 0×1001\nMessageEndpoint= 0×01\nMessageContent= \"55534243000000000000000000000011060000000000000000000000000000\""
 }
 
 # or
