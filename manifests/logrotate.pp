ec36df817c987f3699cbb9b23c6c49558fd6d97e
Greg Cope <gregcope@gmail.com>
2014-12-20 10:07:29 +0000

    Added GPS logging

--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -1,11 +1,5 @@
 class piboatmon::logrotate {
 
-  # add a logrotate
-  file { '/etc/logrotate.d/boatmon':
-    ensure => present,
-    content => "/home/pi/piboatmon/files/boatmon.log {\n\tweekly\n\trotate 31\n\tmissingok\n\tnotifempty\n\tcompress\n\tnocreate\n}\n",
-  }
-
   # enabled logcompression
   file { '/etc/logrotate.d/compress':
     ensure => present,
--- manifests/piboatmon.pp
+++ manifests/piboatmon.pp
@@ -24,4 +24,10 @@ class piboatmon::boatmon {
     source => '/home/pi/piboatmon/manifests/rc.local',
   }
 
+  # add a logrotate for piboatmon
+  file { '/etc/logrotate.d/boatmon':
+    ensure => present,
+    content => "/home/pi/piboatmon/files/boatmon.log\n/home/pi/piboatmon/files/gpspipe.log {\n\tdaily\n\trotate 31\n\tmissingok\n\tnotifempty\n\tcompress\n\tnocreate\n}\n",
+  }
+
 }
