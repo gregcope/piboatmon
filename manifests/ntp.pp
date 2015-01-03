48edda43af6aa4a15956164511c6e837a454f086
Greg Cope <gregcope@gmail.com>
2015-01-03 11:36:02 +0000

    Ntp fixes

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -12,16 +12,30 @@ class piboatmon::ntp {
 # http://www.catb.org/gpsd/gpsd-time-service-howto.html
   exec { 'configNtpGpsServer':
     logoutput => true,
-    command => '/usr/bin/perl -p -i -e "s#server 127.127.28.0.*#server 127.127.28.0 minpoll 4 maxpoll 4 iburst prefer#" /etc/ntp.conf', 
+    command => '/usr/bin/perl -p -i -e "s#server 127\.127\.28\.0.*#server 127\.127\.28\.0 minpoll 4 maxpoll 4 iburst prefer#" /etc/ntp.conf', 
     unless => '/bin/grep "server 127.127.28.0 minpoll 4 maxpoll 4 iburst prefer" /etc/ntp.conf',
     notify => Service [ 'ntp' ],
+    require => Exec [ 'addGpsServer' ],
   }
 
   exec { 'configNtpGpsFudge':
     logoutput => true,
-    command => '/usr/bin/perl -p -i -e "s#fudge 127.127.28.0.*#fudge 127.127.28.0 time1 +0.340 refid GPS#" /etc/ntp.conf',
+    command => '/usr/bin/perl -p -i -e "s#fudge 127\.127\.28\.0.*#fudge 127\.127\.28\.0 time1 \+0\.340 refid GPS#" /etc/ntp.conf',
     unless => '/bin/grep "fudge 127.127.28.0 time1 +0.340 refid GPS" /etc/ntp.conf',
     notify => Service [ 'ntp' ],
+    require => Exec [ 'addGpsFudge' ],
+  }
+
+  exec { 'addGpsFudge':
+    logoutput => true,
+    command => '/bin/echo "fudge 127.127.28.0" >> /etc/ntp.conf',
+    unless => '/bin/grep "fudge 127.127.28.0" /etc/ntp.conf',
+  }
+
+  exec { 'addGpsServer':
+    logoutput => true,
+    command => '/bin/echo "server 127.127.28.0" >> /etc/ntp.conf',
+    unless => '/bin/grep "server 127.127.28.0" /etc/ntp.conf',
   }
 
 # ntp service
