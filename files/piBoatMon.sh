dfee07864d711d974df3297f95feb910a91227bf
Greg Cope <gregcope@gmail.com>
2015-01-06 20:57:00 +0000

    Reduced syncs/boat time, added GPS logging

--- files/piBoatMon.sh
+++ files/piBoatMon.sh
@@ -4,28 +4,24 @@
 timeOutSecs=90s
 defaultSleepSecs=3600
 
-# try and set the date from GPS to kickoff ntpd
-#/usr/bin/logger -t piBoatMon "Starting gpsDate in the background"
-#/home/pi/piboatmon/files/gpsDate &
-
-# log the ntp status
-/usr/bin/logger -t piBoatMon ntpq status `/usr/bin/ntpq -p | grep NMEA`
-
-# log the battery volts
-/usr/bin/logger -t piBoatMon `/usr/bin/sudo /usr/sbin/mopicli -v1` &
-/usr/bin/logger -t piBoatMon `/usr/bin/sudo /usr/sbin/mopicli -v2` &
-
 /usr/bin/logger -t piBoatMon "Setting the mopi power on timer to default of 3600 secs - piboatmon.py should overwrite it"
 /usr/bin/sudo /usr/sbin/mopicli -won $defaultSleepSecs &
 
-/usr/bin/logger -t piBoatMon "Starting /home/pi/piboatmon/files/piboatmon.py"
 # unleash the python
 /usr/bin/timeout $timeOutSecs /usr/bin/sudo /home/pi/piboatmon/files/piboatmon.py
-/usr/bin/logger -t piBoatMon "Finished /home/pi/piboatmon/files/piboatmon.py"
+
+# lame logrotate
+# takes each copy and copies it into a logfile called piboatmon.log.20150106
+logfile=/home/pi/piboatmon/files/piboatmon.log
+timestamp=`/bin/date +%Y%m%d`
+newlogfile=$logfile.$timestamp
+/bin/cat $logfile >> $newlogfile
+
+# delete the oldest logfiles
+/usr/bin/find /home/pi/piboatmon/files -name piboatmon.log*.tgz -mtime +10 -exec rm {} \;
 
 sleep 60
 
-/usr/bin/logger -t piBoatMon "Going to sleep ... nite sync, mopciki -wsd 1"
 /bin/sync
 
 # force a nice power off in 1 sec
